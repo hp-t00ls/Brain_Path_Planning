a90ddd7c2529575e9c37c148691e0e5f97b24fa0
uczde <167856621+uczde@users.noreply.github.com>
2024-08-24 12:52:08 +0200

    Node algo rewritten from Hipo

---
+++ Path_Planning/Assets/Scripts/Node.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class Node
+{
+    public Vector3 Position;
+    public Node Parent;
+
+    public Node(Vector3 position, Node parent = null)
+    {
+        Position = position;
+        Parent = parent;
+    }
+}
